69bd9447eaa1b1b84eec68a2ab62ba7ff67a4a74
Karthikeya-Thatipamula <karthikeyathatipamula18025@gmail.com>
2025-10-17 23:24:08 +0530

    3:05:22 "Implementaion of logger and middleware done"

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,4 +1,4 @@
-import winston from ('winston');
+import winston from 'winston';
 
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
@@ -22,3 +22,5 @@ if (process.env.NODE_ENV !== 'production') {
     )
   }));
 }
+
+export default logger;
