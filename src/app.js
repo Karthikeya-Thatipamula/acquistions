bf21ceddccb9cab22a0fd0a768d841a70638b7e5
Karthikeya-Thatipamula <karthikeyathatipamula18025@gmail.com>
2025-11-04 01:43:41 +0530

    4:03:11 'Secured the developed api with arcjet'

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ import logger  from '#config/logger.js';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import { timestamp } from 'drizzle-orm/gel-core'; 
+import cookieParser from 'cookie-parser'; 
 import authRoutes from './routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
 
@@ -16,6 +16,8 @@ app.use(express.urlencoded({ extended: true}));
 app.use(cookieParser());
 
 app.use(morgan('combined', { stream: {write: (message) => logger.info(message.trim()) }}));
+
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');
